4076498f2b7ab544f4006ada8ef7711df7e0e128
Ahmad Ainul Rizki <ahmad@tanahatas.me>
2019-08-25 07:40:04 +0700

    ✅ UPDATE: thumbnail.vue

--- components/thumbnail.vue
+++ components/thumbnail.vue
@@ -33,24 +33,19 @@ export default {
   width: 100%;
   height: 100%;
   perspective: 1000px;
-
   background-color: white;
 }
-
 .projects-image > img {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
-
 .projects-card-wrapper {
   position: relative;
-
   border-radius: 0.1rem;
   overflow: hidden;
   cursor: url('/cnblack.svg'), auto;
 }
-
 .projects-image {
   width: 100%;
   height: 100%;
@@ -58,17 +53,15 @@ export default {
   transform-origin: center;
   transition: transform 1.25s cubic-bezier(0.475, 0.425, 0, 0.995);
 }
-
 .projects-card-wrapper {
   position: relative;
   transition: transform 0.95s cubic-bezier(0.475, 0.425, 0, 0.995);
 }
-
 .projects-content {
   position: absolute;
   right: -4%;
   text-align: right;
-  bottom: 9%;
+  bottom: 3%;
   display: inline-block;
   z-index: 10;
   overflow: hidden;
@@ -77,18 +70,15 @@ export default {
   transition: all 0.45s cubic-bezier(0.475, 0.425, 0, 0.995);
   color: rgb(31, 31, 31);
 }
-
 .projects-wrapper-top {
   transition: all 1s;
 }
 .projects-wrapper-top:hover .projects-content {
   right: -2%;
 }
-
 .projects-content:active {
   color: aquamarine;
 }
-
 .type-sub2 {
   font-weight: 700;
   font-size: 32px;
@@ -96,25 +86,21 @@ export default {
   letter-spacing: 0.02em;
   margin-bottom: 0px;
 }
-
 .type-sub3 {
   font-size: 16px;
   line-height: 32px;
   letter-spacing: 0.05em;
   margin-top: 1px;
 }
-
 /* animate */
 .projects-wrapper-top:hover .projects-card-wrapper {
   transition-delay: 100ms;
   transform: scale(0.95) rotateY(-9deg);
 }
-
 .projects-wrapper-top:hover .projects-card-wrapper .projects-image {
   transition-delay: 200ms;
   transform: scale(1);
 }
-
 @media screen and (max-width: 800px) {
   .type-sub2[data-v-5e3ebf7e] {
     font-size: 27px;
